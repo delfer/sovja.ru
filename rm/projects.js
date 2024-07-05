58605a21caebc925349c2046c12429c676247550
Alexander Chumakov <i@delfer.ru>
2024-07-05 13:01:10 +0300

    Create projects.js

---
+++ rm/projects.js
@@ -0,0 +1,56 @@
+        const projects = [
+            {
+                "title": "Speedeybee Bluetooth",
+                "photos": ["img/IMG_0858.jpeg", "https://via.placeholder.com/150"],
+                "tasks": [
+                    { "task": "Разработка", "completed": true, "subtasks": [] },
+                    { "task": "Выпуск первой партии", "completed": true, "subtasks": [] },
+                    { "task": "Продажа", "completed": false, "subtasks": [
+                        { "task": "Публикация на Авито", "completed": true },
+                        { "task": "Регистрация ЮЛ", "completed": false },
+                         { "task": "Публикация на OZON", "completed": false },
+                          { "task": "Публикация на Wildberries", "completed": false },
+                    ] }
+                ]
+            },
+            {
+                "title": "USB-UART Autoswitch",
+                "photos": ["img/photo_2024-07-05_12-51-37.jpg", "img/photo_2024-07-05_12-52-23.jpg", "img/photo_2024-07-05_12-53-06.jpg"],
+                "tasks": [
+                    { "task": "Схема", "completed": true, "subtasks": [] },
+                    { "task": "Дизайн ПП", "completed": true, "subtasks": [] },
+                    { "task": "Написание прошивки", "completed": true, "subtasks": [
+                  ] }
+                ]
+            },
+            {
+                "title": "Speedeybee USB",
+                "photos": ["img/photo_2024-07-05_12-50-25.jpg", "photo_2024-07-05_12-52-03.jpg"],
+                "tasks": [
+                    { "task": "Task 1.1", "completed": false, "subtasks": [] },
+                    { "task": "Task 1.2", "completed": true, "subtasks": [
+                        { "task": "Subtask 1.2.1", "completed": false }
+                    ] }
+                ]
+            },
+            {
+                "title": "AT32 FC",
+                "photos": ["https://via.placeholder.com/150", "https://via.placeholder.com/150"],
+                "tasks": [
+                    { "task": "Task 2.1", "completed": true, "subtasks": [] },
+                    { "task": "Task 2.2", "completed": false, "subtasks": [
+                        { "task": "Subtask 2.2.1", "completed": true }
+                    ] }
+                ]
+            },
+            {
+                "title": "Drone combat",
+                "photos": ["https://via.placeholder.com/150", "https://via.placeholder.com/150"],
+                "tasks": [
+                    { "task": "Task 2.1", "completed": true, "subtasks": [] },
+                    { "task": "Task 2.2", "completed": false, "subtasks": [
+                        { "task": "Subtask 2.2.1", "completed": true }
+                    ] }
+                ]
+            }
+        ];
